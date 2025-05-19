c9f7cb29a25f29de91bbec08d187e95b390f8a66
Дмитрий Николанков <d.nikola1704@gmail.com>
2025-05-20 00:04:53 +0300

    cors fix

--- src/main/java/io/github/sersul/feedcollector/security/SecurityConfig.java
+++ src/main/java/io/github/sersul/feedcollector/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package io.github.sersul.feedcollector.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -32,6 +33,10 @@ import java.util.List;
 public class SecurityConfig {
     private final UserDetailsService userDetailsService;
 
+
+    @Value("${APP_CORS_ALLOWED_ORIGINS}")
+    private String allowedOrigins;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -57,7 +62,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "localhost:3000", "http://frontend"));
+        String[] origins = allowedOrigins.split(",");
+        configuration.setAllowedOrigins(List.of(origins));
         configuration.addAllowedMethod("*");
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // для сессий и куки
