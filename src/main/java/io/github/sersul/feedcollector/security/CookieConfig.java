9fde890da9cd4a4dbeeb9e6570554cc165fe9613
Дмитрий Николанков <d.nikola1704@gmail.com>
2025-05-20 00:33:26 +0300

    nginx cookie fix

---
+++ src/main/java/io/github/sersul/feedcollector/security/CookieConfig.java
@@ -0,0 +1,11 @@
+package io.github.sersul.feedcollector.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
+
+@Configuration
+public class CookieConfig {
+
+}
