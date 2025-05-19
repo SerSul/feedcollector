f1e1e7388e34e959316151f1ecebd1fe8e709c91
Дмитрий Николанков <d.nikola1704@gmail.com>
2025-05-19 23:56:28 +0300

    nginx

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api/v1',
+    baseURL: '/api', // Относительный путь для проксирования через Nginx
     withCredentials: true,
     headers: {
-        'Content-Type': 'application/json',
-    },
+        'Content-Type': 'application/json'
+    }
 });
 
 export default api;
