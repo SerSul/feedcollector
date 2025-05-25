f297b26edc006dbc07dccca6c019ed0206f88cac
Дмитрий Николанков <d.nikola1704@gmail.com>
2025-05-26 00:12:13 +0300

    time fix

--- src/main/java/io/github/sersul/feedcollector/entity/BaseEntity.java
+++ src/main/java/io/github/sersul/feedcollector/entity/BaseEntity.java
@@ -8,6 +8,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 /**
  * Базовый класс для всех сущностей с общими полями
@@ -25,11 +26,11 @@ public abstract class BaseEntity {
 
     @CreatedDate
     @Column(name = "created_at", updatable = false)
-    private LocalDateTime createdAt;
+    private ZonedDateTime createdAt;
 
     @LastModifiedDate
     @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
+    private ZonedDateTime updatedAt;
 
     /**
      * Проверяет, является ли сущность новой (еще не сохраненной в БД)
